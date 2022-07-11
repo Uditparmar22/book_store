86a6cafc4f12ab9ac01cf51ae7ff239d21c76ad5
Uditparmar22 <parmarudit2002@gmail.com>
2022-07-11 13:26:55 +0530

    First commit Message

---
+++ BookStore/BookStore.Models/ViewModels/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BookStore.Models.ViewModels
+{
+    public partial class Role
+    {
+        public Role()
+        {
+            Users = new HashSet<User>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+        public string Results { get; internal set; }
+        public int TotalRecords { get; internal set; }
+    }
+}
